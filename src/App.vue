6ec22dd0a72b6e271743ba3bdb6f4a5c3f0c0547
dufeng <2517838529@qq.com>
2019-07-20 21:05:24 +0800

    aa

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <!-- 顶部 Header 区域 -->
+    <mt-header fixed title=" Vue项目" class="header"></mt-header>
+
+    <!-- 中间的 路由 router-view 区域  主要是路由的内容-->
+<transition mode='out-in' >
+      <router-view>
+
+
+
+      </router-view>
+</transition>
+  
+
+    <!-- 底部 TabBar 区域 -->
+    <nav class="mui-bar mui-bar-tab">
+      <router-link to="/home" class="mui-tab-item ">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
+      <router-link to="/member" class="mui-tab-item">
+        <span class="mui-icon mui-icon-email"></span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link to="/shopcar" class="mui-tab-item">
+        <span class="mui-icon mui-icon-home mui-icon-extra-cart mui-icon-extra">
+          <span class="mui-badge">0</span>
+        </span>
+
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
+      <router-link to="/search" class="mui-tab-item">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
+     
+
+
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+
+<style lang="scss" scoped>
+
+ 
+	.v-enter, 
+	.v-leave-to {
+		opacity: 0;
+	}
+	.v-enter {
+		transform: translateX(100%);
+	}
+	.v-leave-to {
+		transform: translateX(-100%);
+		position: absolute;
+	}
+	.v-enter-active,
+	.v-leave-active {
+		transition: all .4s ease;
+	}
+
+  
+#app{
+  padding-top: 40px;
+  	overflow-x: hidden;
+}
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,32 @@
+import VueRouter from 'vue-router'
+
+
+// 导入对应的路由组件
+import HomeContainer from './components/tabbar/homeContainer.vue'
+import MemberContainer from './components/tabbar/memberContainer.vue'
+import ShopcarContainer from './components/tabbar/shopcarContainer.vue'
+import SearchContainer from './components/tabbar/shearchContainer.vue'
+import newList from './components/news/newsList.vue'
+import newInfo from './components/news/newInfo.vue'
+import phot0List from './components/photos/photoList.vue'
+
+
+// 3. 创建路由对象
+var router = new VueRouter({
+  routes: [
+    { path: '/home', component: HomeContainer },
+    { path: '/member', component: MemberContainer },
+    { path: '/shopcar', component: ShopcarContainer },
+    { path: '/search', component: SearchContainer },
+    { path: '/home/newList', component: newList },
+    { path: '/home/newInfo/:id', component: newInfo },
+    { path: '/home/photoList', component: phot0List },
+
+  ],
+  // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
+  linkActiveClass:'mui-active'
+  
+})
+
+// 把路由对象暴露出去
+export default router
